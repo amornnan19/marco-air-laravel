2dfbb1ce09e301e13ef72e64bcb9206a93f8ef50
amornnan <165276621+amornnan19@users.noreply.github.com>
2025-07-26 14:59:37 +0700

    feat: add ProductSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserRoleSeeder::class,
             PromotionSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
